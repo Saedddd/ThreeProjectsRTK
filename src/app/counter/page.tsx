6512de30b97fc9a4a3940b4359e6e3ec857a93ef
Saedakhram <102800804+Saedddd@users.noreply.github.com>
2024-01-23 23:16:49 +0600

    Add reducer increment

--- src/app/counter/page.tsx
+++ src/app/counter/page.tsx
@@ -1,18 +1,19 @@
 "use client"
 
 
-import { useSelector } from "react-redux";
+import { increment } from "@/features/counterSlice/ui/counterSlice";
+import { useDispatch, useSelector } from "react-redux";
 
 
 
 const Counter = () => {
   const counter = useSelector((state : any) => state.counter.value)
-
+  const dispatch = useDispatch()
 
   return (
     <main className="w-full h-screen bg-black text-white">
       <div className="text-center py-[10rem] flex items-center gap-8 justify-center">
-        <button  className="btn btn-outline btn-primary">
+        <button onClick={() => dispatch(increment())} className="btn btn-outline btn-primary">
           Increment
         </button>
         <span>{counter}</span>
--- src/features/counterSlice/ui/counterSlice.tsx
+++ src/features/counterSlice/ui/counterSlice.tsx
@@ -5,16 +5,20 @@ import { createSlice, PayloadAction} from "@reduxjs/toolkit";
 import { ICount } from "../types";
 
 const initialState : ICount = {
-    value: 100,
+    value: 0,
 }
 
 export const counterSlice = createSlice({
     name: "counter",
     initialState,
     reducers:{
-
+        increment: (state) =>{
+            state.value += 1
+        },
     }
 })
+
+export const {increment} = counterSlice.actions; 
 
 export const CounterReducer = counterSlice.reducer;
 

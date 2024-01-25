9bb48a19928db7f01cece4aebf3ba7dc3680d7c0
Saedakhram <102800804+Saedddd@users.noreply.github.com>
2024-01-25 20:31:18 +0600

    Update main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,29 +3,17 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex bg-black min-h-screen flex-col items-center justify-between p-24">
-        <div className="collapse bg-base-200">
-            <input type="checkbox" /> 
-            <div className="collapse-title text-xl font-medium">
-              Click me to show/hide content
-            </div>
-            <div className="collapse-content"> 
-              <p>hello</p>
-            </div>
+       <div className="diff aspect-[16/9]">
+          <div className="diff-item-1">
+            <div className="bg-primary text-primary-content text-9xl font-black grid place-content-center">World</div>
+          </div>
+          <div className="diff-item-2">
+            <div className="bg-base-200 text-9xl font-black grid place-content-center">Hello</div>
+          </div>
+          <div className="diff-resizer"></div>
         </div>
 
 
-        <div className="">
-        <label className="swap swap-flip  text-9xl">
-  
-          <input type="checkbox" />
-          
-          <div className="swap-on">😈</div>
-          <div className="swap-off">😇</div>
-
-         
-        </label>
-
-        </div>
 
        
     </main>

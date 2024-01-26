24a220b29ccce8add5422b201e479e1db1375b42
Saedakhram <102800804+Saedddd@users.noreply.github.com>
2024-01-26 20:46:28 +0600

    Add del task btn

--- src/features/DelleteEditTask/ui/DeleteEditTask.tsx
+++ src/features/DelleteEditTask/ui/DeleteEditTask.tsx
@@ -10,22 +10,27 @@ import Modal from '@/shared/ui/Modal';
 
 const DeleteEditTask:React.FC<TaskProps> = ({task}) => {
     const [isOpen, setIsOpen] = useState<boolean>(false)
+    const [isOpenDel, setIsOpenDel] = useState<boolean>(false)
     const [todoValue, setTodoValue] = useState<string>(task.text)
 
 
-    const handleEditSubmit: FormEventHandler<HTMLFormElement> = (e) => {
+    const handleEditSubmit: FormEventHandler<HTMLFormElement> = async (e) => {
         e.preventDefault()
 
         setIsOpen(false)
     }
 
+    const handleDeleteTask = async (id : string) => {
+        setIsOpenDel(false)
+    }
 
   return (
     <>
         <tr key={task.id}>
             <td className='w-full'>{task.text} TOdo</td>
             <td  className='flex gap-5 cursor-pointer'>
-                <FiTrash2 onClick={() => setIsOpen(true)} />
+
+                <FiEdit onClick={() => setIsOpen(true)} />
                 
                 <Modal
                     isOpen={isOpen}
@@ -51,7 +56,30 @@ const DeleteEditTask:React.FC<TaskProps> = ({task}) => {
 
                 </Modal>
 
-                <FiEdit />
+                <FiTrash2 onClick={() => setIsOpenDel(true)} />
+
+
+                <Modal
+                    isOpen={isOpenDel}
+                    onClose={()=> setIsOpenDel(false)}
+
+                >
+                    <h3 className='text-lg'>
+                        Are you sure, you want to delete this task?
+                    </h3>
+                    <div className='modal-action'>
+                        <button onClick={() => handleDeleteTask(task.id)} className='btn'>
+                        Yes
+                        </button>
+                     </div>
+
+                </Modal>
+
+
+
+
+
+               
             </td>
         </tr>
 

20390d2ad532257520896672eec4ac482b1b68d6
Saedakhram <102800804+Saedddd@users.noreply.github.com>
2024-01-30 21:46:03 +0600

    Configure TasksApiSlice

--- src/shared/api/TasksApiSlice.tsx
+++ src/shared/api/TasksApiSlice.tsx
@@ -1,9 +1,21 @@
-import { CreateApi, fetchBaseQuery } from "@reduxjs/toolkit/query";
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 
-export const tasksApi = new createApi({
+type TTasks = {
+    id: string;
+    text: string;
+}
+
+export const tasksApi = createApi({
     reducerPath: 'tasks',
     baseQuery: fetchBaseQuery({
-        baseUrl: "http://localhost:3001/tasks"
+        baseUrl: "http://localhost:3001/"
     }),
-})
+    endpoints: (builder) => ({
+        getAllTasks: builder.query<TTasks, any>({
+            query: () => "tasks",
+        })
+    })
+});
+
+export const {useGetAllTasksQuery} = tasksApi

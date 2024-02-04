e88c7a013f7140d8b98f5704ad1cc3e827c8c629
Saedakhram <102800804+Saedddd@users.noreply.github.com>
2024-02-04 20:52:12 +0600

    Update AddTodoForm

--- src/features/AddTodoForm/ui/AddTodoForm.tsx
+++ src/features/AddTodoForm/ui/AddTodoForm.tsx
@@ -11,19 +11,22 @@ const AddTodoForm = () => {
 	};
 
 	return (
-		<form onSubmit={onSubmit} className='form-inline mt-3 mb-3'>
-			<label className='sr-only'>Name</label>
-			<input
-				type='text'
-				className='form-control mb-2 mr-sm-2'
-				placeholder='Add todo...'
-				value={value}
-				onChange={(event) => setValue(event.target.value)}
-			></input>
-
-			<button type='submit' className='btn btn-primary mb-2'>
-				Submit
-			</button>
+		<form onSubmit={onSubmit} className='form-inline mt-3 mb-3 flex gap-4 items-center'>
+			<div className="">
+				<label className='sr-only'>Name</label>
+				<input
+					type='text'
+					className='form-control mb-2 mr-sm-2 input input-bordered input-primary w-full max-w-xs'
+					placeholder='Add todo...'
+					value={value}
+					onChange={(event) => setValue(event.target.value)}
+				></input>
+			</div>
+			<div className="">
+				<button type='submit' className='btn btn-primary mb-2'>
+					Submit
+				</button>
+			</div>
 		</form>
 	);
 };

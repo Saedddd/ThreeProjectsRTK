aadd4968b09a2a56cbaee29ff7c830176b776db6
Saedakhram <102800804+Saedddd@users.noreply.github.com>
2024-02-21 16:47:00 +0600

    Create btn cart and fav

--- src/shared/ui/Button/types/index.ts
+++ src/shared/ui/Button/types/index.ts
@@ -1,6 +1,6 @@
 export interface IBtn {
-    onClick: () => void;
-    type: 'submit' | 'button' | undefined;
-    className: string;
-    children: React.ReactNode;
+  onClick?: () => void;
+  type?: "submit" | "button" | undefined;
+  className?: string;
+  children?: React.ReactNode;
 }
--- src/widgets/Header/ui/Header.tsx
+++ src/widgets/Header/ui/Header.tsx
@@ -1,25 +1,45 @@
-import Link from 'next/link'
-import React from 'react'
+"use client";
+
+import Button from "@/shared/ui/Button";
+import Modal from "@/shared/ui/Modal";
+import Link from "next/link";
+import React, { useState } from "react";
 
 const Header = () => {
+  const [isOpenCart, setIsOpenCart] = useState<boolean>(false);
+  const [isOpenFav, setIsOpenFav] = useState<boolean>(false);
   return (
     <div className="w-[70%] mx-auto">
-    <ul className="absolute right-0 left-0 top-[3rem] flex items-center justify-center gap-[2rem]">
-      <li>
-        <Link href="/">Home</Link>
-      </li>
-      <li>
-        <Link href="/counter">Counter</Link>
-      </li>
-      <li>
-        <Link href="/todo">Todo List</Link>
-      </li>
-      <li>
-        <Link href="/shop">Shopping</Link>
-      </li>
-    </ul>
-  </div>
-  )
-}
+      <ul className="absolute right-0 left-0 top-[3rem] flex items-center justify-center gap-[2rem]">
+        <li>
+          <Link href="/">Home</Link>
+        </li>
+        <li>
+          <Link href="/counter">Counter</Link>
+        </li>
+        <li>
+          <Link href="/todo">Todo List</Link>
+        </li>
+        <li>
+          <Link href="/shop">Shopping</Link>
+        </li>
 
-export default Header
+        <Button onClick={() => setIsOpenCart(true)} type="button">
+          Cart
+        </Button>
+        <Modal isOpen={isOpenCart} onClose={() => setIsOpenCart(false)}>
+          <div className="modal-actions">fsdfdsf</div>
+        </Modal>
+
+        <Button onClick={() => setIsOpenFav(true)} type="button">
+          Likes
+        </Button>
+        <Modal isOpen={isOpenFav} onClose={() => setIsOpenFav(false)}>
+          <div className="modal-actions">fsdfdsf</div>
+        </Modal>
+      </ul>
+    </div>
+  );
+};
+
+export default Header;

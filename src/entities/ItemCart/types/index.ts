e59f95187f1d3565c4a3ec4e84dbbb392b96b912
Saedakhram <102800804+Saedddd@users.noreply.github.com>
2024-02-23 15:13:59 +0600

    Fix bug

--- src/entities/ItemCart/types/index.ts
+++
@@ -1,5 +0,0 @@
-import ICardItem from "@/entities/CardItem/types";
-
-export default interface ICartItem {
-  cards: ICardItem[];
-}
---
+++ src/entities/ItemCart/ui/ItemCart.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+import ICardItem from "@/entities/CardItem/types";
+
+const ItemCart = ({ img, title, id, desc, price }: ICardItem) => {
+  return (
+    <div className="card w-96 bg-base-100 shadow-xl image-full">
+      <figure>
+        <img src={img} alt="Shoes" />
+      </figure>
+      <div className="card-body">
+        <h2 className="card-title">{title}</h2>
+        <p>{desc}</p>
+        <p>{price}</p>
+        <div className="card-actions justify-end">
+          <button className="btn btn-primary">Buy Now</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ItemCart;

31174494ac3a2f24f55cafe82daa52ff512ee954
Saedakhram <102800804+Saedddd@users.noreply.github.com>
2024-02-25 19:33:32 +0600

    Update itemCart

--- src/entities/ItemCart/ui/ItemCart.tsx
+++ src/entities/ItemCart/ui/ItemCart.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 
+import Image from "next/image";
 import ICardItem from "@/entities/CardItem/types";
 
 const ItemCart = ({ img, title, id, desc, price }: ICardItem) => {
   return (
     <div className="card w-96 bg-base-100 shadow-xl image-full">
       <figure>
-        <img src={img} alt="Shoes" />
+        <Image src={img} alt="Shoes" />
       </figure>
       <div className="card-body">
         <h2 className="card-title">{title}</h2>
